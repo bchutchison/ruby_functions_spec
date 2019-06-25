b8fe1240b7f0a6a8e55aa0a73ba8aa16dcd3aa11
bchutchison <benhutchison85@gmail.com>
2019-06-25 15:16:03 +0100

    add string as number

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -6,3 +6,29 @@ end
 def return_10()
   return 10
 end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(x, y)
+ return x * y
+end
+
+def divide(x, y)
+  return x / y
+end
+
+def length_of_string(x)
+  return x.length
+end
+
+
+def join_string(x, y)
+  return x + y
+end
+
+
+def add_string_as_number(x, y)
+  return x.to_i + y.to_i
+end
